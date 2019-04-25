e5cc8c9f8a8c2f2485c6124cfca3b8b5d2b52489
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-04-25 12:10:51 +0300

    add server user register route and controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,13 +1,49 @@
 const User = require('../models/users');
 
 exports.getUsers = function(req, res) {
-  User.find({})
-        .exec((errors, users) => {
-
+  User.find({}).exec((errors, users) => {
     if (errors) {
-      return res.status(422).send({errors});
+      return res.status(422).send({ errors });
     }
 
     return res.json(users);
   });
-}
+};
+
+exports.register = function(req, res) {
+  const registerData = req.body;
+
+  if (!registerData.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required'
+      }
+    });
+  }
+
+  if (!registerData.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required'
+      }
+    });
+  }
+
+  if (registerData.password !== registerData.passwordConfirmation) {
+    return res.status(422).json({
+      errors: {
+        password: 'is not the same as confirmation password'
+      }
+    });
+  }
+
+  const user = new User(registerData);
+
+  return user.save((errors, savedUser) => {
+    if (errors) {
+      return res.status(422).json({ errors });
+    }
+
+    return res.json(savedUser);
+  });
+};
--- server/models/users.js
+++ server/models/users.js
@@ -1,23 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Meetup = require('./meetups');
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const config = require('../config/dev')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const config = require('../config/dev');
 
 const userSchema = new Schema({
   avatar: String,
-  email: { type: String,
-           required: 'Email is Required',
-           lowercase: true,
-           unique: true,
-           match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]},
-  name: { type: String,
-          required: true,
-          min: [6, 'Too short, min is 6 characters']},
-  username: { type: String,
-          required: true,
-          min: [6, 'Too short, min is 6 characters']},
+  email: {
+    type: String,
+    required: 'Email is Required',
+    lowercase: true,
+    unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+  },
+  name: {
+    type: String,
+    required: true,
+    min: [6, 'Too short, min is 6 characters']
+  },
+  username: {
+    type: String,
+    required: true,
+    min: [6, 'Too short, min is 6 characters']
+  },
   password: {
     type: String,
     min: [4, 'Too short, min is 4 characters'],
@@ -30,29 +36,43 @@ const userSchema = new Schema({
   joinedMeetups: [{ type: Schema.Types.ObjectId, ref: 'Meetup' }]
 });
 
-userSchema.pre("save", function(next){
-   const user = this;
+userSchema.pre('save', function(next) {
+  const user = this;
 
-   bcrypt.genSalt(10, function(err, salt) {
-      if(err){ return next(err);}
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
 
-      bcrypt.hash(user.password, salt, function(err, hash){
-          if(err){ return next(err);}
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
 
-          user.password = hash;
-          next();
-      });
-   });
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 //Every user have acces to this methods
-userSchema.methods.comparePassword = function(candidatePassword, callback){
-   bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
-      if(err) {return callback(err);}
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
 
-      callback(null, isMatch);
-   });
-}
+    callback(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', userSchema);
 
 
-module.exports = mongoose.model('User', userSchema );
+// {
+//   "username": "Alex",
+//   "name": "Alexander Bryksin",
+//   "email": "alexander.bryksin@yandex.ru",
+//   "password": "123456",
+//   "passwordConfirmation": "123456",
+// }
