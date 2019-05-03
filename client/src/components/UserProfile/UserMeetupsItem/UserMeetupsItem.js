b0ac90b394262e573b18981e8a07ec56afd6fa3f
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-05-03 13:44:49 +0300

    add UserProfile MeetupsItem component

---
+++ client/src/components/UserProfile/UserMeetupsItem/UserMeetupsItem.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const UserMeetupsItem = ({ meetup }) => {
+  return (
+    <div className="column is-3-tablet is-6-mobile">
+      {/*Threads*/}
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img src={meetup.image} />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">{meetup.title}</p>
+              <p className="subtitle is-6">
+                <span className="tag is-dark subtitle">
+                  {meetup.category.name}
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="content">
+            <p>{meetup.shortInfo}</p>
+          </div>
+        </div>
+        <footer className="card-footer">
+          <a className="card-footer-item">Share</a>
+          <a className="card-footer-item">Delete</a>
+        </footer>
+      </div>
+      <br />
+    </div>
+  );
+};
+
+export default UserMeetupsItem;
--- client/src/pages/PageProfile/PageProfile.js
+++ client/src/pages/PageProfile/PageProfile.js
@@ -9,8 +9,12 @@ import {
   userPostsSelector,
   userThreadsSelector
 } from '../../ducks/user';
+import { userSelector } from '../../ducks/auth';
+import Loader from '../../components/shared/Loader/Loader';
+import UserMeetupsItem from '../../components/UserProfile/UserMeetupsItem/UserMeetupsItem';
 
 const PageProfile = ({
+  user,
   userPosts,
   userThreads,
   userMeetups,
@@ -23,72 +27,52 @@ const PageProfile = ({
     fetchUserStats();
   }, []);
 
+  if (!user) return <Loader />;
+
   return (
     <div className="columns">
       <div className="container profile">
-        <div className="section profile-heading">
-          <div className="columns is-mobile is-multiline">
-            <div className="column is-2">
-              <figure className="image  header-icon user-profile-image">
-                <img className="is-rounded" src="" />
-              </figure>
-            </div>
-            <div className="column is-4-tablet is-10-mobile name">
-              <p>
-                <span className="title is-bold">Alex Example</span>
-                <br />
-                <button className="button is-primary is-outlined m-t-sm">
-                  Update Info
-                </button>
-                <br />
-              </p>
-              <p className="tagline">
-                I am very productive and good programmer
-              </p>
-            </div>
-            <div className="stats-tab column is-2-tablet is-4-mobile has-text-centered">
-              <p className="stat-val">2</p>
-              <p className="stat-key">Meetups</p>
-            </div>
-            <div className="stats-tab column is-2-tablet is-4-mobile has-text-centered">
-              <p className="stat-val">4</p>
-              <p className="stat-key">Threads</p>
-            </div>
-            <div className="stats-tab column is-2-tablet is-4-mobile has-text-centered">
-              <p className="stat-val">3</p>
-              <p className="stat-key">Posts</p>
-            </div>
-          </div>
-        </div>
-        <div className="columns is-mobile is-multiline">
-          <div className="column is-3-tablet is-6-mobile">
-            {/*Threads*/}
-            <div className="card">
-              <div className="card-image">
-                <figure className="image is-4by3">
-                  <img src="" />
+        {user ? (
+          <div className="section profile-heading">
+            <div className="columns is-mobile is-multiline">
+              <div className="column is-2">
+                <figure className="image  header-icon user-profile-image">
+                  <img className="is-rounded" src={user.avatar} />
                 </figure>
               </div>
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-content">
-                    <p className="title is-4">Nice Meetup</p>
-                    <p className="subtitle is-6">
-                      <span className="tag is-dark subtitle">Sport</span>
-                    </p>
-                  </div>
-                </div>
-                <div className="content">
-                  <p>Some short info</p>
-                </div>
+              <div className="column is-4-tablet is-10-mobile name">
+                <p>
+                  <span className="title is-bold">{user.name}</span>
+                  <br />
+                  <button className="button is-primary is-outlined m-t-sm">
+                    Update Info
+                  </button>
+                  <br />
+                </p>
+                <p className="tagline">{user.info}</p>
               </div>
-              <footer className="card-footer">
-                <a className="card-footer-item">Share</a>
-                <a className="card-footer-item">Delete</a>
-              </footer>
+              <div className="stats-tab column is-2-tablet is-4-mobile has-text-centered">
+                <p className="stat-val">{meetupsCount}</p>
+                <p className="stat-key">Meetups</p>
+              </div>
+              <div className="stats-tab column is-2-tablet is-4-mobile has-text-centered">
+                <p className="stat-val">{threadsCount}</p>
+                <p className="stat-key">Threads</p>
+              </div>
+              <div className="stats-tab column is-2-tablet is-4-mobile has-text-centered">
+                <p className="stat-val">{postsCount}</p>
+                <p className="stat-key">Posts</p>
+              </div>
             </div>
-            <br />
           </div>
+        ) : (
+          <Loader />
+        )}
+        <div className="columns is-mobile is-multiline">
+          {userMeetups &&
+            userMeetups.map((meetup) => (
+              <UserMeetupsItem meetup={meetup} key={meetup._id} />
+            ))}
         </div>
         <div className="columns is-mobile is-multiline">
           {/*Threads map*/}
@@ -135,6 +119,7 @@ const PageProfile = ({
 
 export default connect(
   (state) => ({
+    user: userSelector(state),
     userPosts: userPostsSelector(state),
     userThreads: userThreadsSelector(state),
     userMeetups: userMeetupsSelector(state),
